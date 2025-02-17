4a6efee80fde83dde2235ce8c5b50b534e1f17b1
Breno Rosa <71451784+Akunozz@users.noreply.github.com>
2025-02-17 17:42:46 -0300

    criação página

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup>
+import sidebar from '~/components/flowbite/sidebar.vue';
+import navbar from '~/components/flowbite/navbar.vue';
+</script>
+
+<template>
+
+  <sidebar />
+  <navbar />
+  
+  <div class="p-4 sm:ml-64 bg-black mt-16 sm:mt-14">
+    
+    <slot />
+
+  </div>
+</template>
