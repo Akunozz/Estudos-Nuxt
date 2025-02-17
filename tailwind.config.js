bc813f510b2b3124245fe43272f0f7d45fd97ca8
Breno Rosa <71451784+Akunozz@users.noreply.github.com>
2025-02-17 17:44:58 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,6 @@ export default {
     "./plugins/**/*.{js,ts}",
     "./nuxt.config.{js,ts}",
     "./node_modules/flowbite/**/*.{js,ts}",
-    "./assets/**/*{css}",
   ],
   theme: {
     extend: {},
